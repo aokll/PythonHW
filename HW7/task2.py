515d15cd15227e9ea5281d6deab07d56edabc1cb
aokll <pastuhov_1992@mail.ru>
2023-06-11 11:29:53 +0300

    version 7.03

--- HW7/task2.py
+++ HW7/task2.py
@@ -16,3 +16,13 @@
 # 4 8 12 16 20 24
 # 5 10 15 20 25 30
 # 6 12 18 24 30 36
+
+def print_operation_table(operation,num_rows,num_columns):   
+    for i in range(1,num_rows+1):
+        print()
+        for j in range(1,num_columns+1):
+            print(operation(i,j), end="   ")
+
+print_operation_table(lambda x, y: x * y,6,6)
+
+
